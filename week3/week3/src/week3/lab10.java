c755a9cf42d40a28757d420e27aef55683b33c6e
suksuk555 <mu.suthichai_st@tni.ac.th>
2022-11-24 16:32:47 +0700

    java week3

--- week3/week3/src/week3/lab10.java
+++ week3/week3/src/week3/lab10.java
@@ -1,9 +1,30 @@
 package week3;
+import java.text.*;
+import java.util.*;
 
 public class lab10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final double COMMISSION_RATE = 0.15;
+		final int SENTINEL = -1;
+		
+		DecimalFormat frm = new DecimalFormat("#,##0.00");
+		Scanner sc = new Scanner(System.in);
+		int sale;
+		
+		double salary = 0;
+		do {
+			System.out.print("enter sales in dollors (or -1 to end)");
+			sale = sc.nextInt();
+			if(sale!=-1) {
+				salary = 1000 + sale * COMMISSION_RATE;
+				System.out.println("Salary is: $"+frm.format(salary));
+				
+			}
+			else {
+				System.out.println("bye");
+			}
+		}while(sale!=-1);
 
 	}
 
